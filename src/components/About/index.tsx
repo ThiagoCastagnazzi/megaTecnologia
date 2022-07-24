befd41435b9aba5a720281410451e97db45e82fc
ThiagoCastagnazzi <thiagocastagnazzi@gmail.com>
2022-07-24 00:40:54 -0400

    fix: adjust about styles

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "reactstrap";
 const About = () => {
   return (
     <section className="section bg-light" id="about">
-      <Container>
+      <Container className="about-container">
         <Row className="justify-content-center">
           <Col lg={6} md={8}>
             <div className="title text-center mb-5">
@@ -18,13 +18,11 @@ const About = () => {
             </div>
           </Col>
         </Row>
-        <Row>
-          <Col md={4}>
-            <h2 className="font-weight-light line-height-1_6 text-dark mb-4">
-              Integração de Tecnologias
-            </h2>
-          </Col>
-          <Col md={{ size: 7, offset: 1 }}>
+        <Row className="about-descrip-row">
+          <h2 className="font-weight-light line-height-1_6 text-dark mb-4">
+            Integração de <span className="text-warning">Tecnologias</span>
+          </h2>
+          <Col md={{ size: 7, offset: 1 }} className="about-descrip-container">
             <Row>
               <Col md={6}>
                 <p className="text-muted font-weight-light">
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "../../styles/globals.css";
 import "../../styles/header.css";
 import "../../styles/product.css";
 import "../../styles/service.css";
+import "../../styles/about.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
